5d365566da147d5abb997d695f3275589b7927d1
pasgiannis <gp519@ic.ac.uk>
2020-04-10 23:04:29 +0300

    Add project files.

---
+++ test project/first.cpp
@@ -0,0 +1,11 @@
+#include "first.h"
+#include <iostream>
+#include <string>
+int main() {
+
+	std::cout << "Yolo swag";
+	std::cout << "Yolo swag";
+	int hi;
+	std::string there = "there";
+	std::cout << "hi"<<there;
+}
